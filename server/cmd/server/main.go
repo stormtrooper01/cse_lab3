f7e6cf718fe6c15e496fd161cded43b0acd5e6c3
Yelena <lena.skripnik.01@gmail.com>
2020-12-13 18:23:52 +0200

    Adding to main.go

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -1,1 +1,51 @@
+package main
+
+import (
+    "database/sql"
+    "flag"
+    "github.com/stormtrooper01/cse-lab3/server/db"
+    "log"
+    "net/http"
+    "os"
+    "os/signal"
+)
+
+var httpPortNumber = flag.Int("p", 8000, "HTTP Port Number")
+
+func NewDbConnection() (*sql.DB, error) {
+  c := &db.Connection{
+    Host: "localhost",
+    User: "postgres",
+    Password: "121212",
+    DbName: "db",
+    DisableSSL: true,
+  }
+  return c.Open()
+}
+
+func main() {
+  flag.Parse()
+
+  server, err := ComposeApiServer(HttpPortNumber(*httpPortNumber))
+  if err == nil {
+    go func() {
+      log.Println("Starting server")
+      err := server.Start()
+      if err == http.ErrServerClosed {
+        log.Printf("HTTP server stopped")
+      } else {
+        log.Fatalf("Cannot start HTTP server: %s", err)
+      }
+    }()
+
+    sigChannel := make(chan os.Signal, 1)
+    signal.Notify(sigChannel, os.Interrupt)
+    <-sigChannel
+    if err := server.Stop(); err != nil && err != http.ErrServerClosed {
+      log.Printf("Error stopping the server: %s", err)
+    }
+  } else {
+    log.Fatalf("Cannot initialize banking server: %s", err)
+  }
+}
 
