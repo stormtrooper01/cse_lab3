4e4a37ca25361377ff36af300a2f7bf99cb2eb1e
Yelena <lena.skripnik.01@gmail.com>
2020-12-13 18:27:01 +0200

    Update db.go

--- server/db/db.go
+++ server/db/db.go
@@ -1,1 +1,33 @@
+package db
 
+import (
+	"database/sql"
+	"net/url"
+	_ "github.com/lib/pq"
+)
+
+type Connection struct {
+  Host string
+  User, Password string
+  DbName string
+  DisableSSL bool
+}
+
+func (c *Connection) ConnectionURL() string {
+  dbUrl := &url.URL{
+    Scheme: "postgres",
+    User: url.UserPassword(c.User, c.Password),
+    Host: c.Host,
+    Path: c.DbName,
+  }
+  if c.DisableSSL {
+    dbUrl.RawQuery = url.Values{
+      "sslmode": []string{"disable"},
+    }.Encode()
+  }
+  return dbUrl.String()
+}
+
+func (c *Connection) Open() (*sql.DB, error) {
+  return sql.Open("postgres", c.ConnectionURL())
+}
