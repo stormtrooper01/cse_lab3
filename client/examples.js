bde4778968386edffeebdd0cee635a252113bc56
SonyaSytnik <sofia.sytnik2012@gmail.com>
2020-12-14 18:43:01 +0200

    Update examples.js

--- client/examples.js
+++ client/examples.js
@@ -1,1 +1,28 @@
+'use strict';
+const scenarios = require('./scenarios/client');
 
+const client = scenarios.clientApi('http://localhost:8000');
+
+// Scenario 1: Display available accounts.
+client.accountsList()
+    .then(list => {
+        console.log('-----Scenario 1-----');
+        console.log('Available accounts: ');
+        console.dir(list);
+    })
+    .catch(e => {
+        console.log(Problem listing available accounts: ${e.message});
+    });
+
+// Scenario 2: Make a transaction
+client.transaction({ giverId: 2, receiverId: 1, sum: 2000.14 })
+  .then(res => {
+    console.log('-----Scenario 2-----');
+    console.log('Create transaction response:', res);
+    return client.accountsList()
+    .then(list => list.map(c => c.name).join(', '));
+    console.log('Transaction completed');
+  })
+  .catch(e => {
+      console.log(Problem creating a new transaction: ${e.message});
+  });
