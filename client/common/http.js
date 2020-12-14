89591660c8466fbd0a07c689d3fb1644f3263a3e
SonyaSytnik <sofia.sytnik2012@gmail.com>
2020-12-14 18:39:21 +0200

    Add http file

--- client/common/http.js
+++ client/common/http.js
@@ -1,1 +1,38 @@
+'use strict';
 
+const request = require('request');
+
+const client = baseUrl => {
+  const respHandler = response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(Unexpected response from the server ${response.status} ${response.statusText})
+  };
+  return {
+    get: path => {
+      return new Promise((resolve, reject) => {
+        request(${baseUrl}${path}, { json: true }, (err, res, body) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(body);
+        });
+      });
+    },
+    post: async (path, data) => {
+      return new Promise((resolve, reject) => {
+        request(${baseUrl}${path}, { json: true, method: 'POST', body: data }, (err, res, body) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(body);
+        });
+      });
+    }
+  };
+};
+
+module.exports = { client };
