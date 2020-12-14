4d0d130587ea04bf9d6001cab4b9eda738313d6f
SonyaSytnik <sofia.sytnik2012@gmail.com>
2020-12-14 18:41:06 +0200

    Update client.js

--- client/scenarios/client.js
+++ client/scenarios/client.js
@@ -1,1 +1,12 @@
+'use strict';
+const http = require('../common/http');
 
+const clientApi = baseUrl => {
+  const client = http.client(baseUrl);
+  return {
+    accountsList: () => client.get('/banking'),
+    transaction: ({ giverId, receiverId, sum }) => client.post('/banking', { giverId, receiverId, sum })
+  }
+};
+
+module.exports = { clientApi };
